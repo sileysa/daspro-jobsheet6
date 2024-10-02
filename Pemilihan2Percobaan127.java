e786bd45de5917dee49c4fbf7616f78a43b269b6
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-10-02 13:43:21 +0700

    saya upload Pemilihan2Percobaan127 ke daspro-jobsheet6

--- Pemilihan2Percobaan127.java
+++ Pemilihan2Percobaan127.java
@@ -8,10 +8,10 @@ public class Pemilihan2Percobaan127 {
         tahun = input27.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat");
+            if ((tahun % 1000) != 0)
+                System.out.println("Bukan Tahun Kabisat");
         } else 
-            System.out.println("Bukan Tahun Kabisat");
+            System.out.println("Tahun Kabisat");
             
     }
 }
