5a47003976a7d75a767e2e28026bfc91a37e8f33
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-10-08 20:12:30 +0700

    saya upload modifikasi program ke daspro-jobsheet6

--- Pemilihan2Percobaan127.java
+++ Pemilihan2Percobaan127.java
@@ -6,12 +6,19 @@ public class Pemilihan2Percobaan127 {
         int tahun;
         System.out.print("Masukkan tahun: ");
         tahun = input27.nextInt();
-
+    
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-        } else 
-            System.out.println("Bukan Tahun Kabisat");
+            } else {
+                if (tahun % 400 == 0) 
+                    System.out.println("Tahun Kabisat");
+                else
+                    System.out.println("Bukan Tahun Kabisat"); 
+                } 
+        } else {
+                System.out.println("Bukan Tahun Kabisat");
+        }
             
     }
 }
